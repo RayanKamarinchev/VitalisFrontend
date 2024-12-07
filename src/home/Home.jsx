928e4f043638aae40bed1ad92c2d7e0edd2216c6
Rayan Kamarinchev <rayan2008bg@gmail.com>
2024-12-07 19:52:29 +0200

    working conversion to .smi format

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,9 +1,13 @@
 import React, {useEffect} from 'react';
+import axios from "axios";
 
 const Home = () => {
+  const apiBaseUrl = process.env.REACT_APP_API_BASE_URL
+  
   const onLoad = () => {
     if (window.marvin && window.marvin.Sketch) {
       window.marvin.sketcherInstance = new window.marvin.Sketch("sketch");
+      window.marvin.Sketch.license("")
     } else {
       console.log("Cannot initiate sketcher. Current browser may not support HTML5 canvas or may run in Compatibility Mode.");
     }
@@ -25,6 +29,10 @@ const Home = () => {
       console.log("gui.nocache.js loaded");
       setTimeout(() => {
         onLoad();
+        let sketch = document.getElementById("sketch")
+        if(sketch) {
+          sketch.id = "usedSketcher"
+        }
       }, 1000);
     });
     
@@ -34,10 +42,30 @@ const Home = () => {
     };
   }, []);
   
+  const resRef = React.useRef(null);
+  
+  async function getMol() {
+    let res = await window.marvin.sketcherInstance.exportStructure("mrv")
+    //TODO: handle undefined res
+    await console.log(res)
+    await axios.post("https://openbabel.cheminfo.org/v1/convert", {
+      input: res,
+      inputFormat: "mrv -- Chemical Markup Language",
+      outputFormat: "smi -- SMILES format"
+    }).then((res) => {
+      let result = res.data.result
+      console.log(result)
+      resRef.current.innerText = result.substring(0, result.indexOf('\t'))
+    })
+    
+  }
+  
   return (
       <div>
         <p>hello</p>
         <div id="sketch" style={{height: "500px", width: "500px"}}></div>
+        <button onClick={getMol}>Get Mol</button>
+        <p id="res" ref={resRef}></p>
       </div>
   );
 };
